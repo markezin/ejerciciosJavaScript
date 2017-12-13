af4129c9bc0c81f4bbcc4a7fdf49ecd6e2885245
Unknown <carlosmarquezlorenzo@hotmail.com>
2017-12-13 12:58:59 +0100

    First Function

--- js/ejercicioCarlos.js
+++ js/ejercicioCarlos.js
@@ -8,26 +8,68 @@ var i;
 // Process--------------------------------------------
 
 
-for ( i = 1; i < 100 ; i++ ){
+// for ( i = 1; i < 100 ; i++ ){
 
-	if (i % 3 == 0) {
+// 	if (i % 3 == 0) {
 
-		console.log("fizz");
+// 		console.log("fizz");
 
+// 	}
+
+// 	else if (i % 5 == 0) {
+
+// 		console.log("buzz");
+
+// 	}
+
+// 	else {
+// 		console.log(i);
+// 	}
+
+// }
+
+
+
+
+
+
+function divisiblePor(n1,n2){
+		
+	if (n2 % n1 == 0) {
+		return true;
 	}
-
-	else if (i % 5 == 0) {
-
-		console.log("buzz");
-
+	else{
+		return false;
 	}
-
-	else {
-		console.log(i);
-	}
-
 }
 
+
+
+	// if(divisiblePor(3,i))
+	// {
+	// 	console.log("Fizz");
+	// }
+	// else
+	// {
+	// 	console.log("No lo es");
+	// }
+
+
+
+for (i=1; i < 100; i++)
+{
+	if(divisiblePor(3,i)) {
+		console.log("FIZZ")
+	}
+
+	else if(divisiblePor(5,i)) {
+		console.log("BUZZ")
+	}
+		else{
+			console.log(i)
+		}
+
+}
 
 
 
@@ -40,9 +82,6 @@ for ( i = 1; i < 100 ; i++ ){
 
 
 
-
-
-	---------------------------------------------------------------------------
 
 
 
