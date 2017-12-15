96a4710079dea927f8290968da367a10df195eca
Unknown <carlosmarquezlorenzo@hotmail.com>
2017-12-15 11:48:20 +0100

    Interruptor con imágenes

--- js/ejercicioInterruptor.js
+++ js/ejercicioInterruptor.js
@@ -1,1 +1,32 @@
-function
+var myCabron = true;
+
+function toggle() {
+
+var myTarget = document.getElementById("myButton");
+
+// Si el INTERRUPTOR está apagado...
+
+if( myCabron == true )
+{
+	myCabron = false; // ... encienda el INTERRUPTOR
+
+	myTarget.style.backgroundColor = "darkgreen";
+
+	myTarget.innerHTML = "JoPuta";
+
+	myImage.src = "img/barSimpson.png";
+
+}
+
+// Si no...
+	else
+	{
+		myCabron = true; // ... apague el INTERRUPTOR
+
+		myTarget.style.backgroundColor = "darkred";
+
+		myTarget.innerHTML = "Cabrón";
+
+		myImage.src = "img/GokuVegeta.png";
+	}
+}
